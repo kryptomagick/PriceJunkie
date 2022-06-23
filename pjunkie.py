95af09af99754ab6b3facc097971c12b01f45b5b
pvial <pvial@thin>
2022-06-23 10:57:01 -0700

    edit

--- pjunkie.py
+++ pjunkie.py
@@ -10,17 +10,24 @@ def getCurrentPrice():
 
 class PriceJunkie(object):
     direction = 0
+    directionInc = 1
     avg = 0.0
     interval = 60
     nextPrice = 0.0
     nextFigure = 0.0
+    lastNext = 0.0
     avgNext = 0.0
-    hourOut = 0.0
+    shortOut = 0.0
+    longOut = 0.0
+    headed = 0.0
     high = 0
     low = 0
     lastPrice = 0.0
     currentPrice = 0.0
     total = 0.0
+    sampleWins = 0
+    winsPercent = 0.0
+    winRange = 20
     priceHistory = []
     def guessNextPrice(self):
         diff = self.currentPrice - self.lastPrice
@@ -39,17 +46,18 @@ class PriceJunkie(object):
     def run(self):
         c = 1
         while True:
-            self.lastPrice = self.currentPrice
             self.currentPrice = getCurrentPrice()
+            if (self.lastNext < (self.currentPrice + self.winRange)) and (self.lastNext > (self.currentPrice - self.winRange)):
+                self.sampleWins += 1
             self.total += self.currentPrice
             self.avg = self.total / c
             diff = self.currentPrice - (self.lastPrice)
             if diff < 0:
                 diff *= -1
             if self.currentPrice > self.lastPrice:
-                self.direction += 1
+                self.direction += self.directionInc
             else:
-                self.direction -= 1
+                self.direction -= self.directionInc
             if self.direction > self.high:
                 self.peak = self.direction
             if self.direction < self.low:
@@ -57,9 +65,12 @@ class PriceJunkie(object):
             self.nextFigure = self.guessNextPrice()
             self.avgNext += self.nextFigure / c
             self.nextPrice = self.currentPrice - self.nextFigure
-            self.hourOut = self.guessHourPrice()
+            self.headed = self.guessHourPrice()
+            self.winsPercent = self.sampleWins / c
             c += 1
-            print("price: ", self.currentPrice, "lastPrice: ", self.lastPrice, "avg: ", self.avg, "nextPrice: ", self.nextPrice, "direction: ", self.direction, "diff: ", diff, "nextFigure: ", self.nextFigure, "hourOut: ", self.hourOut)
+            print("price: ", self.currentPrice, "lastPrice: ", self.lastPrice, "avg: ", self.avg, "nextPrice: ", self.nextPrice, "direction: ", self.direction, "diff: ", diff, "nextFigure: ", self.nextFigure, "headed: ", self.headed, "Wins: ", self.sampleWins, "Wins Percent: ", self.winsPercent)
+            self.lastPrice = self.currentPrice
+            self.lastNext = self.nextPrice
             sleep(self.interval)
 
 pjunkie = PriceJunkie()
